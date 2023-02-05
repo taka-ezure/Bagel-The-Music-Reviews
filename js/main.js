b8d7ac9db96cd87212a7c8b2cf958a63afa7e741
taka-ezure <fy4.s2020@gmail.com>
2023-02-05 19:05:10 +0900

    test

--- js/main.js
+++ js/main.js
@@ -1,49 +1,19 @@
 const app = Vue.createApp({
     data: () => ({
-        images: [
-            {
-                index: 1,
-                src: "img/radiohead/1.png"
-            },
-            {
-                index: 2,
-                src: "img/radiohead/2.png"
-            },
-            {
-                index: 3,
-                src: "img/radiohead/3.png"
-            },
-            {
-                index: 4,
-                src: "img/radiohead/4.png"
-            }, {
-                index: 5,
-                src: "img/radiohead/5.png"
-            }, {
-                index: 6,
-                src: "img/radiohead/6.png"
-            }, {
-                index: 7,
-                src: "img/radiohead/7.png"
-            },
-            {
-                index: 8,
-                src: "img/radiohead/8.png"
-            }
-        ]
     })
 })
 app.mount('#app')
+
 // luxy.init();
 
-$(function(){
-    $('header, main, footer').inertiaScroll({
-      parent: $("#wrap")
+// $(function(){
+//     $('header, main, footer').inertiaScroll({
+//       parent: $("#wrap")
 
-      //オプションを追加する場合はここに追記
+//       //オプションを追加する場合はここに追記
 
-    });
-  });
+//     });
+//   });
 
 
 $(function () {
