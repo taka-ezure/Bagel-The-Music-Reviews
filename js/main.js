cf7172db80d6f354f5cda94cefdae5758aa2262c
taka-ezure <fy4.s2020@gmail.com>
2023-03-04 16:47:32 +0900

    latest

--- js/main.js
+++ js/main.js
@@ -12,3 +12,16 @@
     });
 });
 
+/*===================== Navbarのコーディング ===================== */
+$('.menu-trigger').on('click',function(){
+    if($(this).hasClass('active')){
+      $(this).removeClass('active');
+      $('nav').removeClass('open');
+      $('.overlay').removeClass('open');
+    } else {
+      $(this).addClass('active');
+      $('nav').addClass('open');
+      $('.overlay').addClass('open');
+    }
+  });
+  /*===================== Navbarのコーディング ===================== */
