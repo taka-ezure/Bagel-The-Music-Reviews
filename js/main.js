08d1740f480a9888b8953ccd59129a17b5fa9603
taka-ezure <fy4.s2020@gmail.com>
2023-03-11 18:50:04 +0900

    latest

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
   $(function () {
     $('.btn-more').on('click', function () {
         $('body').css('overflow', 'hidden');  // 本文の縦スクロールを無効
+        $('.modal-bg').css('overflow', 'hidden');  // 本文の縦スクロールを無効
         $('.modal').fadeToggle();
         var sclTop = $(window).scrollTop();
         $('.inner').offset({ top: (sclTop + 40)});
@@ -9,6 +10,7 @@
     $('.btn-modal').on('click', function () {
         $('.modal').fadeToggle();
         $('body').css('overflow','auto');     // 本文の縦スクロールを有効
+        $('.modal-bg').css('overflow', 'auto');  // 本文の縦スクロールを無効
     });
 });
 
