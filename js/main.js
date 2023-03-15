641aef5dc9146a84a117205d6271f92af423cb9d
taka-ezure <fy4.s2020@gmail.com>
2023-03-15 19:16:39 +0900

    latest

--- js/main.js
+++ js/main.js
@@ -1,29 +1,32 @@
-  $(function () {
-    $('.btn-more').on('click', function () {
-        $('body').css('overflow', 'hidden');  // 本文の縦スクロールを無効
-        $('.modal-bg').css('overflow', 'hidden');  // 本文の縦スクロールを無効
-        $('.modal').fadeToggle();
-        var sclTop = $(window).scrollTop();
-        $('.inner').offset({ top: (sclTop + 40)});
-    });
+var sclTop
+$(function () {
+  $('.btn-more').on('click', function () {
+    $('.modal').fadeToggle();
+    $('body').css('overflow', 'hidden');  // 本文の縦スクロールを無効
+    // $('body').css('position', 'fixed');  // 本文の縦スクロールを無効
+    // $('.modal').css('display', 'block');  // 本文の縦スクロールを無効
+    sclTop = $(window).scrollTop();
+    $('.inner').offset({ top: (sclTop + 40) });
+  });
 
-    $('.btn-modal').on('click', function () {
-        $('.modal').fadeToggle();
-        $('body').css('overflow','auto');     // 本文の縦スクロールを有効
-        $('.modal-bg').css('overflow', 'auto');  // 本文の縦スクロールを無効
-    });
+  $('.btn-modal').on('click', function () {
+    $('.modal').fadeToggle();
+    $('body').css('overflow', 'auto');     // 本文の縦スクロールを有効
+    // $('body').css('position', 'static'); // 本文の縦スクロールを無効
+    // $(window).scrollTop(sclTop);  // 本文の縦スクロールを無効
+  });
 });
 
 /*===================== Navbarのコーディング ===================== */
-$('.menu-trigger').on('click',function(){
-    if($(this).hasClass('active')){
-      $(this).removeClass('active');
-      $('nav').removeClass('open');
-      // $('.overlay').removeClass('open');
-    } else {
-      $(this).addClass('active');
-      $('nav').addClass('open');
-      // $('.overlay').addClass('open');
-    }
-  });
+$('.menu-trigger').on('click', function () {
+  if ($(this).hasClass('active')) {
+    $(this).removeClass('active');
+    $('nav').removeClass('open');
+    // $('.overlay').removeClass('open');
+  } else {
+    $(this).addClass('active');
+    $('nav').addClass('open');
+    // $('.overlay').addClass('open');
+  }
+});
   /*===================== Navbarのコーディング ===================== */
