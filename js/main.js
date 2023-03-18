b763061e0b19844505f4fec5e3493f6439e93349
taka-ezure <fy4.s2020@gmail.com>
2023-03-18 21:36:53 +0900

    latest

--- js/main.js
+++ js/main.js
@@ -2,17 +2,20 @@ var sclTop
 $(function () {
   $('.btn-more').on('click', function () {
     $('.modal').fadeToggle();
-    $('body').css('overflow', 'hidden');  // 本文の縦スクロールを無効
     // $('body').css('position', 'fixed');  // 本文の縦スクロールを無効
     // $('.modal').css('display', 'block');  // 本文の縦スクロールを無効
     sclTop = $(window).scrollTop();
+    // $('body').css('overflow', 'hidden');  // 本文の縦スクロールを無効
+		$('body').addClass('fixed').css({'top': sclTop});
     // $('.inner').offset({ top: (sclTop + 40) });
     $('.inner').offset({ top: (sclTop + '2%') });
   });
 
   $('.btn-modal').on('click', function () {
     $('.modal').fadeToggle();
-    $('body').css('overflow', 'auto');     // 本文の縦スクロールを有効
+    // $('body').css('overflow', 'auto');     // 本文の縦スクロールを有効
+    $('body').removeClass('fixed').css({'top': 0});
+    window.scrollTo( 0 , sclTop );
     // $('body').css('position', 'static'); // 本文の縦スクロールを無効
     // $(window).scrollTop(sclTop);  // 本文の縦スクロールを無効
   });
